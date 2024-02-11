08e3c38a43171c1db11534456737e118ab8c7837
francesr1001 <127363327+francesr1001@users.noreply.github.com>
2024-02-11 11:37:26 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -59,10 +59,10 @@
 
                                         
                                         <hr>
-                                        <a href="Index.html" class="btn btn-google btn-user btn-block">
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
-                                        <a href="Index.html" class="btn btn-facebook btn-user btn-block">
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                                         </a>
                                     </form>
